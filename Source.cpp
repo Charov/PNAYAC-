54450a03a3d6aa40d719abc9088255c0d8630737
Charov <artyr13531@mail.ru>
2018-12-12 16:48:19 +0300

    Add files via upload

--- Source.cpp
+++ Source.cpp
@@ -4,97 +4,43 @@
 
 using namespace std;
 
-void InsertToFrom(vector<string>& to, const vector<string>& from)
+bool IsPalindrom(string s)
 {
-	to.insert(end(to), begin(from), end(from));
+	string word;
+	for (int i = s.size() - 1; i >= 0; --i)
+	{
+		word += s[i];
+	}
+	if (s == word)
+		return true;
+	else
+		return false;
 }
 
-void Remove(int day, vector < vector < string>>& v, string& delet)
+vector<string> PalindromFilter(vector<string> words, int minLength)
 {
-	for (int i = 0; i < v[day].size(); i++)
+	vector<string> result;
+	for (string i : words)
 	{
-		if (v[day][i] == delet)
+		if (i.size() >= minLength)
 		{
-			v[day].erase(v[day].begin() + i);
+			if (IsPalindrom(i))
+				result.push_back(i);
 		}
 	}
+	return result;
 }
 
 int main()
 {
-	setlocale(LC_ALL, "rus");
-	int kolichestvo;
-	int month = 0;
-	vector<vector<string>> v(31);
-	string command;
-	cout << "Введите количество команд: ";
-	cin >> kolichestvo;
-	while (kolichestvo)
+	vector<string> input = { "qwwq", "qwerty", "asdsa", "asd", "zxccxz" };
+	vector<string> output;
+	output = PalindromFilter(input, 4);
+	for (string i : output)
 	{
-		cout << "Введите команду(NEXT-Следующий месяц, DUMP-Вывести, ADD-Добавить, REMOVE-Удалить): ";
-		cin >> command;
-		if (command == "NEXT")
-		{
-			++month;
-			if (month > 11)
-				month = 0;
-			if (month == 1)
-			{
-				for (int i = 28; i<31; ++i)
-				{
-					InsertToFrom(v[27], v[i]);
-				}
-				v.resize(28);
-			}
-			else if (month == 7 || month == 0)
-			{
-			}
-			else
-			{
-				if (v.size() == 30 || v.size() == 28)
-				{
-					v.resize(31);
-				}
-				else
-				{
-					InsertToFrom(v[29], v[30]);
-					v.resize(30);
-				}
-			}
-		}
-		else
-		{
-			int day;
-			cout << "Введите день: ";
-			cin >> day;
-			if (day <= v.size() && day > 0)
-			{
-				--day;
-				if (command == "DUMP")
-				{
-					cout << v[day].size() << " ";
-					for (auto i : v[day])
-						cout << i << " ";
-					cout << "\n";
-				}
-				else if (command == "ADD")
-				{
-					string task;
-					cout << "Введите дело: ";
-					cin >> task;
-					v[day].push_back(task);
-				}
-				else if (command == "REMOVE")
-				{
-					string delet;
-					cout << "Введите дело: ";
-					cin >> delet;
-					Remove(day, v, delet);
-				}
-			}
-		}
-		--kolichestvo;
+		cout << i << " ";
 	}
+	cout << endl;
 	system("pause");
 	return 0;
 }
