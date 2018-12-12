10bb3d81c707fc4a95a45d5991f7412331aee494
Charov <artyr13531@mail.ru>
2018-12-12 16:54:22 +0300

    Update Source.cpp

--- Source.cpp
+++ Source.cpp
@@ -1,37 +1,37 @@
 #include <iostream>
 #include <string>
 #include <vector>
-
-using namespace std;
-
-bool IsPalindrom(string s)
+//#include <algorithm> Äëÿ reverse
+ using namespace std;
+ bool IsPalindrom(string& s)
 {
 	string word;
 	for (int i = s.size() - 1; i >= 0; --i)
 	{
 		word += s[i];
 	}
+	return (s == word);
+}
+ /*bool IsPalindrom(string s)                   reverse
+{
+	string word = s;
+	reverse(s.begin(), s.end());
 	if (s == word)
 		return true;
 	else
 		return false;
-}
-
-vector<string> PalindromFilter(vector<string> words, int minLength)
+}*/
+ vector<string> PalindromFilter(vector<string> words, int minLength)
 {
 	vector<string> result;
 	for (string i : words)
 	{
-		if (i.size() >= minLength)
-		{
-			if (IsPalindrom(i))
-				result.push_back(i);
-		}
+		if (i.size() >= minLength && IsPalindrom(i))
+			result.push_back(i);
 	}
 	return result;
 }
-
-int main()
+ int main()
 {
 	vector<string> input = { "qwwq", "qwerty", "asdsa", "asd", "zxccxz" };
 	vector<string> output;
